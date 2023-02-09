a991fc756ce6ffcd43363f9346a95b368a01ddfd
Anssi Koivusalo <Anssi Koivusalo@ANSSI>
2023-02-09 07:11:48 +0200

    Koodit gittiin

---
+++ Assets/Scripts/Boomerang.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boomerang : MonoBehaviour {
+    private bool initilized = false;
+    private Transform hex;
+
+    private NeuralNetwork net;
+    private Rigidbody2D rBody;
+    private Material[] mats;
+
+    private Quaternion RightAngle = Quaternion.AngleAxis(-20, new Vector3(0, 0, 1));
+    private Quaternion LeftAngle = Quaternion.AngleAxis(20, new Vector3(0, 0, 1));
+
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        mats = new Material[transform.childCount];
+        for(int i = 0; i < mats.Length; i++)
+            mats[i] = transform.GetChild(i).GetComponent<Renderer>().material;
+    }
+
+    void FixedUpdate ()
+    {
+        if (!initilized)
+        {
+            return;
+        }
+
+        //Kästää rayt, saadaan etäisyydet seinistä kolmeen suuntaan
+        RaycastHit2D rayCasthit = Physics2D.Raycast(this.transform.position, rBody.velocity);
+        RaycastHit2D rayCasthitLeft = Physics2D.Raycast(this.transform.position, LeftAngle * rBody.velocity);
+        RaycastHit2D rayCasthitRight = Physics2D.Raycast(this.transform.position, RightAngle * rBody.velocity);
+
+        Debug.DrawRay(this.transform.position, rBody.velocity);
+        Debug.DrawRay(this.transform.position, LeftAngle * rBody.velocity);
+        Debug.DrawRay(this.transform.position, RightAngle * rBody.velocity);
+
+        //eäisyys ja kulma kohteesta
+        float distance = Math.Min(Vector2.Distance(transform.position, hex.position),100);
+
+        for (int i = 0; i < mats.Length; i++)
+            mats[i].color = new Color(distance / 100f, (1f-(distance / 100f)), (1f - (distance / 100f))); 
+
+        float[] inputs = new float[5];
+
+
+        float angle = transform.eulerAngles.z % 360f;
+        if (angle < 0f)
+            angle += 360f;
+
+        Vector2 deltaVector = (hex.position - transform.position).normalized;
+   
+
+        float rad = Mathf.Atan2(deltaVector.y, deltaVector.x);
+        rad *= Mathf.Rad2Deg;
+
+        rad = rad % 360;
+        if (rad < 0)
+        {
+            rad = 360 + rad;
+        }
+
+        rad = 90f - rad;
+        if (rad < 0f)
+        {
+            rad += 360f;
+        }
+        rad = 360 - rad;
+        rad -= angle;
+        if (rad < 0)
+            rad = 360 + rad;
+        if (rad >= 180f)
+        {
+            rad = 360 - rad;
+            rad *= -1f;
+        }
+
+        //Syötetään kulma ja etäisyys kohteesta, sekä säiteiden osumaetäisyydet seinistä verkolla sisään.
+        //Fitnessiin summataan etäisyys kohteesta, kulma sekä etäisyyksien käänteisluvut seinistä --> pienempi fitness on parempi.
+        float raycastDist = 30;
+        float raycastDistLeft = 30;
+        float raycastDistRight = 30;
+
+        FitnessBasedOnCollision(rayCasthit, ref raycastDist);
+        FitnessBasedOnCollision(rayCasthitLeft, ref raycastDistLeft);
+        FitnessBasedOnCollision(rayCasthitRight, ref raycastDistRight);
+			
+        inputs[0] = rad/180f;
+        inputs[1] = distance / 20;
+        inputs[2] = raycastDist / 10;
+        inputs[3] = raycastDistLeft / 10;
+        inputs[4] = raycastDistRight / 10;
+
+        float[] output = net.FeedForward(inputs);
+
+        rBody.velocity = Math.Abs(30f * output[1])* transform.up;
+        rBody.angularVelocity = 200f*output[0];
+        net.AddFitness(Math.Abs(inputs[0]) + inputs[1]);
+	}
+
+    private void FitnessBasedOnCollision(RaycastHit2D rayCastHit, ref float rayDistance)
+    {
+        if (rayCastHit.collider != null)
+        {
+            rayDistance = Vector2.Distance(rayCastHit.point, transform.position);
+            rayDistance = Math.Min(rayDistance, 30);
+
+            net.AddFitness(5f/ rayDistance);
+        }
+    }
+
+    public void setFitness()
+    {
+        net.SetFitness((50f - (Vector2.Distance(transform.position, hex.position))));
+    }
+
+    public void Init(NeuralNetwork net, Transform hex)
+    {
+        this.hex = hex;
+        this.net = net;
+        initilized = true;
+    }
+
+    
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject boomerPrefab;
+    public GameObject hex;
+
+    private bool isTraning = false;
+    private int populationSize = 100;
+    private int generationNumber = 0;
+    private int[] layers = new int[] { 5, 30, 30,20, 2 }; //1 input and 1 output
+    private List<NeuralNetwork> nets;
+    private bool leftMouseDown = false;
+    private List<Boomerang> boomerangList = null;
+
+
+    void Timer()
+    {
+        isTraning = false;
+    }
+
+
+	void Update ()
+    {
+        if (isTraning == false)
+        {
+            if (generationNumber == 0)
+            {
+                InitBoomerangNeuralNetworks();
+            }
+            else
+            {
+                nets.Sort();
+                for (int i = 20; i < populationSize; i++)
+                {
+                    nets[i].CopyWeights(nets[i % 20]);
+                    nets[i].Mutate();
+                }
+
+                for (int i = 0; i < populationSize; i++)
+                {
+                    nets[i].SetFitness(0f);
+                }
+            }
+
+           
+            generationNumber++;
+            
+            isTraning = true;
+            Invoke("Timer",10f);
+            CreateBoomerangBodies();
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            leftMouseDown = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            leftMouseDown = false;
+        }
+
+        if(leftMouseDown == true)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            hex.transform.position = mousePosition;
+        }
+    }
+
+
+    private void CreateBoomerangBodies()
+    {
+        if (boomerangList != null)
+        {
+            for (int i = 0; i < boomerangList.Count; i++)
+            {
+                GameObject.Destroy(boomerangList[i].gameObject);
+            }
+
+        }
+
+        boomerangList = new List<Boomerang>();
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            Boomerang boomer = ((GameObject)Instantiate(boomerPrefab, new Vector3(UnityEngine.Random.Range(30f,50f), UnityEngine.Random.Range(-10, 10f), 0),Quaternion.Euler(new Vector3(0, 0, UnityEngine.Random.Range(0f, 360f))))).GetComponent<Boomerang>();
+            boomer.Init(nets[i],hex.transform);
+            boomerangList.Add(boomer);
+        }
+
+    }
+
+    void InitBoomerangNeuralNetworks()
+    {
+        //population must be even, just setting it to 20 incase it's not
+        if (populationSize % 2 != 0)
+        {
+            populationSize = 20; 
+        }
+
+        nets = new List<NeuralNetwork>();
+        
+        for (int i = 0; i < populationSize; i++)
+        {
+            NeuralNetwork net = new NeuralNetwork(layers);
+            net.Mutate();
+            nets.Add(net);
+        }
+    }
+}
